ad540f4f9e077bccad35b7741204838bd57b4a39
VMBush <71069537+VMBush@users.noreply.github.com>
2022-09-20 04:00:39 +0300

    1

--- hello.py
+++ hello.py
@@ -1,7 +1,6 @@
 def app(environ, start_response):
     data = environ["QUERY_STRING"]
     data = data.replace("&", "\n")
-    print(data)
     data = data.encode("UTF-8")
 
     start_response("200 OK", [
