bbda15653b1ad8421a02254b63778943f4da8faf
VMBush <71069537+VMBush@users.noreply.github.com>
2022-09-20 19:51:10 +0300

    django

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/nginx.conf
 sudo /etc/init.d/nginx restart
 
-gunicorn --bind='0.0.0.0:8080' hello:app
+gunicorn --bind='0.0.0.0:8000' ask.ask.wsgi:application
